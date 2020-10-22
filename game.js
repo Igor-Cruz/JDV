d390ec95cfc0c6b6350c768ddb530bfa01c97802
Igor Cruz <igorcruz.it@outlook.com>
2020-10-21 21:44:50 -0300

    Primeira tentativa, falta a última parte

---
+++ game.js
@@ -0,0 +1,91 @@
+const player1 = "x";
+const player2 = "o";
+var playTime = player2;
+var gameOver = false;
+
+
+atualizaMostrador ();
+inicializarEspacos();
+
+function atualizaMostrador()
+{
+	if (gameOver) {return;}
+
+	if (playTime == player1) 
+	{
+
+		var player = document.querySelectorAll('div#mostrador img') [0];
+		player.setAttribute("src", "IMAGENS/x.png");
+	}
+
+	else
+
+	{
+		var player = document.querySelectorAll('div#mostrador img') [0];
+		player.setAttribute("src", "IMAGENS/o.png");
+
+
+	}
+}
+
+function inicializarEspacos()
+{
+
+	var espacos = document.getElementsByClassName("espaco");
+	for (var i = 0; i < espacos.length; i++)
+	 {
+		espacos[i].addEventListener("click",function ()
+		{
+			if (gameOver) {return;}
+			if (this.getElementsByTagName('img').length == 0)
+
+			{
+				if (playTime == player1) 
+				{
+					this.innerHTML = "<img src = 'IMAGENS/x.png'>";
+					this.setAttribute("jogada", player1);
+					playTime = player2;
+
+				}
+				else
+				{
+					this.innerHTML = "<img src = 'IMAGENS/o.png'>";
+					this.setAttribute("jogada", player2);
+					playTime = player1;
+
+				}
+				atualizaMostrador();
+				verificarVencedor();
+
+
+			} 
+
+		});
+
+			
+
+		
+	}
+
+}
+
+
+
+function verificarVencedor(){
+	var a1 = document.getElementById("a1").getAttribute("jogada");
+	var a2 = document.getElementById("a2").getAttribute("jogada");
+	var a3 = document.getElementById("a3").getAttribute("jogada");
+
+	var b1 = document.getElementById("b1").getAttribute("jogada");
+	var b2 = document.getElementById("b2").getAttribute("jogada");
+	var b3 = document.getElementById("b3").getAttribute("jogada");
+
+	var c1 = document.getElementById("c1").getAttribute("jogada");
+	var c2 = document.getElementById("c2").getAttribute("jogada");
+	var c3 = document.getElementById("c3").getAttribute("jogada");
+
+	var vencedor = "";
+
+	if ((a1 == b1 && a1 == c1 && a1 != "") || (a1 == a2 && a1 == a3 && a1 != "") || (a1 == b2 && a1 == c3 && a1 != "")) 
+		vencedor = a1;
+}
